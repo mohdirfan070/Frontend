3501948e3d59318cdcd88269760376d0d2b49a73
Mohammed Irfan <mohammedirfanrj@gmail.com>
2024-10-09 21:02:36 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-slate-800 w-full h-screen pt-6">
+        <h1 className='text-2xl text-white text-center font-mono'>Hello Irfan</h1>       
+    </div>
+    </>
+  )
+}
+
+export default App
